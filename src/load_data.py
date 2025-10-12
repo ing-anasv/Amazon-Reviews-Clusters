c00a4199d1116f03217bbc975faf50848b863c3b
Ana Sofia <anavalencia.ing@gmail.com>
2025-10-12 01:12:31 -0500

    Initial data exploration

--- src/load_data.py
+++ src/load_data.py
@@ -1,7 +1,11 @@
-#Try 1: Load one file at time
+#Test 1: Load one file at time
 
 import pandas as pd
 from pathlib import Path
+
+#Define root
+root= "amazon-reviews"
+
 
 #Function to read data
 def load_info(file_path):
@@ -12,7 +16,9 @@ def load_info(file_path):
 
     """
 
-    f_path = Path(file_path)
+
+    f_path = "root/file_path"
+    f_path= Path(file_path)
 
     # Check if the path is correct
 
@@ -28,4 +34,17 @@ def load_info(file_path):
     print(f" Loaded {len(df)} rows and {len(df.columns)} columns.")
     return df
 
+
+#Test 2: Identify files in data - raw (first step to read multiple files)
+
+raw_dir = Path("data/raw") #Directory where raw data is
+
+def multiple_files():
+
+    """
+    Lists all the files in the data/raw directory
+
+    """
+    files = list(raw_dir.glob("*.json")) + list(raw_dir.glob("*.json.gz")) #Depending on how the file downloads
+    return sorted(files) #Avoid confussion when loading more data sets
 
